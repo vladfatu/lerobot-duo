38cfb3b404a00a31081a057bed0617b91bdaab10
Vlad Fatu <colegu123@gmail.com>
2025-09-27 22:04:52 +0300

    Updated lerobot

---
+++ test.py
@@ -0,0 +1,10 @@
+from lerobot.find_port import find_port, find_available_ports
+
+
+# finding the port
+find_port()
+
+
+# setup motors
+# import os
+# os.system("poetry run python -m lerobot.setup_motors --robot.type=so101_follower --robot.port=/dev/tty.usbmodem58FD0171361")
